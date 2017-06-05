20144a94818c5a6de4d342c517469bfab9540cf8
MARKIT\bryan.vieregge <bryan.vieregge@markit.com>
2017-06-05 12:51:42 -0500

    Split out the Spend class from Budget

---
+++ MintFinancialExport.Core/Entities/Spend.cs
@@ -0,0 +1,20 @@
+﻿namespace MintFinancialExport.Core.Entities
+{
+    public class Spend
+    {
+        public string St { get; set; }
+        public string Ramt { get; set; }
+        public string IsIncome { get; set; }
+        public string IsTransfer { get; set; }
+        public string IsExpense { get; set; }
+        public string Amt { get; set; }
+        public string Pid { get; set; }
+        public string Type { get; set; }
+        public string Bgt { get; set; }
+        public string Rbal { get; set; }
+        public string Ex { get; set; }
+        public string Cat { get; set; }
+        public string Id { get; set; }
+        public string CatTypeFilter { get; set; }
+    }
+}
